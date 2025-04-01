a23adf6dcd08b88ee40743ba8f1e455ccbf7e198
Usanso <save1086@naver.com>
2025-04-01 16:00:22 +0900

    save
    player 구성 추가

---
+++ textRPG/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textRPG
+{
+    public class Player
+    {
+        private int power;
+        public int Power { get{ return power; } set{ power = value; }  }
+
+        private int speed;
+        public int Speed { get{ return speed; } set{ speed = value; } }
+
+    }
+}
---
+++ textRPG/Scenes/TitleScene.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textRPG.Scenes
+{
+    public class TitleScene : Scene
+    {
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override void Input()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Next()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Result()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SelectChoice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+
+        public override void wait()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
